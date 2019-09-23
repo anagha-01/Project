2fd08d2a4127fd3d2533b6aa5318e17d9a386ac1
anagha=01 <anaghakrishnan98@gmail.com>
2019-09-23 10:30:32 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NavComponent,LoginComponent,AppComponent]
 })
 export class AppModule { }
