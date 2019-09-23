8fb003ddb29f31dc48f2ce5058c27a8e7985587a
anagha=01 <anaghakrishnan98@gmail.com>
2019-09-23 12:02:05 +0530

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes:Routes=[
+  {path:'',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [NavComponent,LoginComponent,AppComponent]
+  bootstrap: [NavComponent,AppComponent]
 })
 export class AppModule { }
